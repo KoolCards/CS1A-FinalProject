ff13f535e8983166a071f0bbcd0b4cd8c0d0eb2c
KoolCards <krissatya09@gmail.com>
2016-11-16 20:11:12 -0800

    commit

---
+++ TestPerson.java
@@ -0,0 +1,12 @@
+/**
+  * Tests class ContactList and PersonList by calling all methods in ContactList and PersonList
+  */
+public class TestPerson {
+	public static void main (String [] arg) {
+		ContactList contactlist1 = new ContactList ("");
+		contactlist1.addperson();
+		person1.read();
+		person1.print();
+		person1.retreive("Smith");
+	}
+}
