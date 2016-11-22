15e0572e39aa6d030b98e09ff0e5fb1ba8a1f5e0
ricardo <ricardoluliwei@gmail.com>
2016-11-21 19:12:22 -0800

    Update ContactList.java

--- ContactList.java
+++ ContactList.java
@@ -1,15 +1,18 @@
 public class ContactList {
 	public void addperson() {
 
-}
+ }
+	public void print() {
+
+ }
 	public void retrive(String lastName) {
 	
-}
+ }
 
 	public void quitAndSave() {
 	
-}
+ }
 	public void open() {
 	
-}
+ }
 }
