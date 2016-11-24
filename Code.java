0d0dc5ccd2e308fd1da29f992009468f8d803d12
ricardo <ricardoluliwei@gmail.com>
2016-11-23 19:30:08 -0800

    Update Code.java

--- Code.java
+++ Code.java
@@ -1,8 +1,9 @@
+
 	public String Search(String newlastname) {
 		String retrievecontactlist = "";
 		for (int i = 0; i < list.size(); i++) {
-			Person temPerson = (Person) list.get(i);
-			String temlastname = temPerson.getlastname();
+			Person temPerson =list.get(i);
+			String temlastname = temPerson.getLastName();
 			if (temlastname.equals(newlastname)) {
 				retrievecontactlist += temPerson + "\n";
 			}
