61d2067ccc8190c8266f67eb8130adc113ea5ad3
ricardo <ricardoluliwei@gmail.com>
2016-11-28 20:08:02 -0800

    Create Save and open Objects.java

---
+++ Save and open Objects.java
@@ -0,0 +1,31 @@
+	public void Save() {
+		FileOutputStream outFile;
+		ObjectOutputStream outObject;
+		try {
+			outFile = new FileOutputStream("data");
+			outObject = new ObjectOutputStream(outFile);
+			outObject.writeObject(myContactList);
+			outFile.close();
+			outObject.close();
+		} catch (IOException ioe) {
+			System.out.println("Error writing objects to the file: " + ioe.getMessage());
+		}
+		 myContactList=null;
+	}
+
+	public void Open() {
+		FileInputStream inputFile;
+		ObjectInputStream inputObject;
+		try {
+			inputFile = new FileInputStream("data");
+			inputObject = new ObjectInputStream(inputFile);
+			myContactList = (ContactList) inputObject.readObject();
+			inputFile.close();
+			inputObject.close();
+		} catch (IOException e) {
+			System.out.println("Error reading object from the file" + e.getMessage());
+		} catch (ClassNotFoundException cnfe) {
+			System.out.println("Error in casting to Rectangle: " + cnfe);
+		}
+	}
+}
