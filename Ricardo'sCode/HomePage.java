f3bf690eb53a03f6788d4f2c33ec7fd2420e741e
ricardo <ricardoluliwei@gmail.com>
2016-12-10 19:35:47 -0800

    Add files via upload

--- Ricardo'sCode/HomePage.java
+++ Ricardo'sCode/HomePage.java
@@ -1,6 +1,10 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
+import java.awt.Point;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,13 +19,23 @@ import java.io.ObjectOutputStream;
 
 import javax.swing.*;
 
-public class HomePage extends JFrame {
-	/**
-	 * 
-	 */
+public class HomePage extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 3636549296071613133L;
-	private static ContactList myContactList;
-
+	private static ContactList myContactList= new ContactList();
+	private static final Point DEFUALT_OPEN_LOCATION=new Point(300, 180);
+	private static void addComp(JPanel thePanel, JComponent comp, int xPos, int yPos, int compWidth, int compHeight, int place, int stretch){
+		GridBagConstraints gridConstraints = new GridBagConstraints();
+		gridConstraints.gridx = xPos;
+		gridConstraints.gridy = yPos;
+		gridConstraints.gridwidth = compWidth;
+		gridConstraints.gridheight = compHeight;
+		gridConstraints.weightx = 100;
+		gridConstraints.weighty = 100;
+		gridConstraints.insets = new Insets(5,5,5,5);
+		gridConstraints.anchor = place;
+		gridConstraints.fill = stretch;
+		thePanel.add(comp, gridConstraints);
+	}
 	public static void main(String[] args) {
 		
 		new HomePage();
@@ -55,8 +69,23 @@ public class HomePage extends JFrame {
 			System.out.println("Error in casting to Rectangle: " + e);
 		}
 	}
+	public void Hide() {
+		setVisible(false);
+	}
+	public void Show() {
+		setVisible(true);
+	}
+	public void actionPerformed(ActionEvent e) {
+		JButton clickedbutton = (JButton) e.getSource();
+		if (clickedbutton.getText() == "Add a new Contact") {
+			new AddContact();
+		} else if (clickedbutton.getText() == "Print the Contact List") {
+			new Print();
+		} else if (clickedbutton.getText() == "Search by lastname") {
+			new Search();
+		}
+	}
 	public HomePage() {
-		myContactList=new ContactList();
 		this.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				save();
@@ -65,36 +94,21 @@ public class HomePage extends JFrame {
 				open();
 			}
 		});
-		class HomeButtonHandler implements ActionListener {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JButton clickedbutton = (JButton) e.getSource();
-				if (clickedbutton.getText() == "Add a new Contact") {
-					new AddContact();
-				} else if (clickedbutton.getText() == "Print the Contact List") {
-					new Print();
-				} else if (clickedbutton.getText() == "Search by lastname") {
 
-				}
-
-			}
-
-		}
 		JPanel homepageJPanel = new JPanel();
 		JButton addButton, printButton, searchButton;
-		HomeButtonHandler homeButtonHandler = new HomeButtonHandler();
 		addButton = new JButton("Add a new Contact");
 		printButton = new JButton("Print the Contact List");
 		searchButton = new JButton("Search by lastname");
-		addButton.addActionListener(homeButtonHandler);
-		printButton.addActionListener(homeButtonHandler);
-		searchButton.addActionListener(homeButtonHandler);
+		addButton.addActionListener(this);
+		printButton.addActionListener(this);
+		searchButton.addActionListener(this);
 		homepageJPanel.add(addButton);
 		homepageJPanel.add(printButton);
 		homepageJPanel.add(searchButton);
 		add(homepageJPanel);
 		setSize(300, 200);
-		setLocation(500, 450);
+		setLocation(300, 180);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
@@ -106,59 +120,63 @@ public class HomePage extends JFrame {
 		private static final long serialVersionUID = -623826465110124110L;
 		private JLabel firstNameLabel, lastNameLabel, streetAddressLabel, emailAddressLabel, phoneLabel,
 		notesLabel;
-	private JTextField firstNameText, lastNameText,streetAddressText, emailAddressText, phoneText;
-	private JTextArea notesText;
+	private JTextField firstNameText, lastNameText,streetAddressText, emailAddressText, phoneText,notesText;
 	private JButton addContact;
 	private String firstName, lastName, streetAddress, phone, email, notes;
-	public AddContact(){
+	public AddContact (){
 		// Create the frame, position it and handle closing it
-		this.setLocationRelativeTo(null);
+		this.setLocation(DEFUALT_OPEN_LOCATION);
 		this.setTitle("Add New Contact");
-		
+		setSize(300, 485);
 		JPanel thePanel = new JPanel();
 		thePanel.setLayout(new GridBagLayout());
 		
 		firstNameLabel = new JLabel("First Name");
 		addComp(thePanel, firstNameLabel, 0, 0, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
 		firstNameText = new JTextField(30);
-		addComp(thePanel, firstNameText, 1, 0, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
+		addComp(thePanel, firstNameText, 0, 2, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
 		
 		lastNameLabel = new JLabel("Last Name");
-		addComp(thePanel, lastNameLabel, 0, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
+		addComp(thePanel, lastNameLabel, 0, 3, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
 		lastNameText = new JTextField(30);
-		addComp(thePanel, lastNameText, 1, 1, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
+		addComp(thePanel, lastNameText, 0, 4, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
 		
 		streetAddressLabel = new JLabel("Street Address");
-		addComp(thePanel, streetAddressLabel, 0, 2, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
+		addComp(thePanel, streetAddressLabel, 0, 5, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
 		streetAddressText = new JTextField(30);
-		addComp(thePanel, streetAddressText, 1, 2, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
+		addComp(thePanel, streetAddressText, 0, 6, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
 		
 		emailAddressLabel = new JLabel("Email Address (ie: you@example.com)");
-		addComp(thePanel, emailAddressLabel, 0, 3, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
+		addComp(thePanel, emailAddressLabel, 0, 7, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
 		emailAddressText = new JTextField(30);
-		addComp(thePanel, emailAddressText, 1, 3, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
+		addComp(thePanel, emailAddressText, 0, 8, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
 		
 		phoneLabel = new JLabel("Phone Number (XXX-XXX-XXXX)");
-		addComp(thePanel, phoneLabel, 0, 4, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
+		addComp(thePanel, phoneLabel, 0, 9, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE);
 		phoneText = new JTextField(30);
-		addComp(thePanel, phoneText, 1, 4, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
+		addComp(thePanel, phoneText, 0, 10, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
 		
 		notesLabel = new JLabel("Notes");
-		addComp(thePanel, notesLabel, 0, 5, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
-		notesText = new JTextArea(8, 30);
-		notesText.setLineWrap(true);
-		notesText.setWrapStyleWord(true);
-		addComp(thePanel, notesText, 0, 5, 4, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
+		addComp(thePanel, notesLabel, 0, 11, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE);
+		notesText = new JTextField(30);
+		addComp(thePanel, notesText, 0, 12, 2, 1, GridBagConstraints.EAST, GridBagConstraints.NONE);
 		addContact = new JButton("Add New Contact");
-		addComp(thePanel, addContact, 2, 20, 1, 1, GridBagConstraints.SOUTHEAST, GridBagConstraints.NONE);
+		addComp(thePanel, addContact, 0, 13, 1, 1, GridBagConstraints.SOUTHEAST, GridBagConstraints.NONE);
 		addContact.addActionListener(this);	
-		
 		this.add(thePanel);
 		// Adjusts the size of the frame to best work for the components
 		this.pack();
 		this.setVisible(true);
 		// Define if the user can resize the frame (true by default)
-		this.setResizable(false);	
+		this.setResizable(false);
+		this.addWindowListener(new WindowAdapter() {
+			public void windowOpened(WindowEvent e) {
+				Hide();
+			}
+			public void windowClosing(WindowEvent e) {
+				Show();
+			}
+		});
    	}
 
    	public void actionPerformed (ActionEvent e){
@@ -169,19 +187,7 @@ public class HomePage extends JFrame {
    		}	
 	// Sets the rules for a component destined for a GridBagLayout
 	// and then adds it 
-	private void addComp(JPanel thePanel, JComponent comp, int xPos, int yPos, int compWidth, int compHeight, int place, int stretch){
-		GridBagConstraints gridConstraints = new GridBagConstraints();
-		gridConstraints.gridx = xPos;
-		gridConstraints.gridy = yPos;
-		gridConstraints.gridwidth = compWidth;
-		gridConstraints.gridheight = compHeight;
-		gridConstraints.weightx = 100;
-		gridConstraints.weighty = 100;
-		gridConstraints.insets = new Insets(5,5,5,5);
-		gridConstraints.anchor = place;
-		gridConstraints.fill = stretch;
-		thePanel.add(comp, gridConstraints);
-	}
+	
 	/*
 	 * Creates a new person object and adds it to Contact List
 	 */
@@ -192,16 +198,45 @@ public class HomePage extends JFrame {
 		 phone= phoneText.getText();
 		 email= emailAddressText.getText();
 		 notes= notesText.getText();
+		 String emailFormat="\\w+@\\w+\\.com";
+
+		 System.out.println(phone);
 		   	if(lastName.equals("")){
 		   		JOptionPane.showMessageDialog(null, "Please enter last name.");
-		   	}else{
+		   	} else if (! email.matches(emailFormat)&&!(email.equals(""))) {
+				JOptionPane.showMessageDialog(null, "Please enter correct email");
+			} else if (!isPhone(phone)&&!(phone.equals(""))) {
+				JOptionPane.showMessageDialog(null, "Please enter correct phone number");
+			}
+		   	else{
+		   		if (!phone.equals("")) {
+		   			StringBuilder stringBuilder=new StringBuilder(phone);
+					 stringBuilder.insert(3, "-");
+					 stringBuilder.insert(7, "-");
+					 phone=stringBuilder.toString();
+				}
 			   	  //create a Person object and pass it to ArrayList to save it
-			   	  Person person = new Person(firstName,lastName, streetAddress, phone, email,notes);
+			   	  Person person = new Person(firstName, lastName, streetAddress, email, phone, notes);
 			   	  myContactList.addperson(person);//Add new person to Array
 			   	  JOptionPane.showMessageDialog(null, person.toString());
 		      }
 		 clear();
 	     }
+	 public boolean isPhone(String phone) {
+		 int flag=0;
+		 char[] phoneCharArray=phone.toCharArray();
+		 for (int i = 0; i < phoneCharArray.length; i++) {
+			if (!Character.isDigit(phoneCharArray[i])) {
+				flag=1;
+			}
+		}
+		if (phone.length()==10) {
+			if (flag==0) {
+				return true;
+			} else return false;	
+		} else return false;
+
+	}
 	 public void clear(){
 		 firstNameText.setText("");
 		 lastNameText.setText("");
@@ -210,22 +245,64 @@ public class HomePage extends JFrame {
 		 emailAddressText.setText("");
 		 notesText.setText("");  	
 		}
+	 
 	}
 	class Print extends JFrame{
+		private TextArea printTextArea;
+		private JPanel jPanel;
 		public  Print() {
-			setSize(600, 400);
+			printTextArea=new TextArea(29,35);
+			jPanel=new JPanel();
+			setSize(300, 485);
+			setLocation(DEFUALT_OPEN_LOCATION);
 			setVisible(true);
-			setLocation(500,500);
-			JPanel jPanel=new JPanel();
-			jPanel.setSize(this.getSize());
-			TextArea textArea=new TextArea(myContactList.PeopleNumber, 6);
-			textArea.setSize(500, 400);
-			textArea.setName("Information");
-			textArea.setText(myContactList.toString());
-			
-			jPanel.add(textArea);
+			jPanel.setSize(getSize());
+			printTextArea.setSize(getSize());
+			printTextArea.setEditable(false);
+			printTextArea.setSize(getSize());
+			printTextArea.setText(myContactList.toString());
+			jPanel.add(printTextArea);
 			add(jPanel);
-			
+			this.addWindowListener(new WindowAdapter() {
+				public void windowOpened(WindowEvent e) {
+					Hide();
+				}
+				public void windowClosing(WindowEvent e) {
+					Show();
+				}
+			});
 		}
 	}
+	class Search extends JFrame implements ActionListener {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -4880328478428515838L;
+		private JTextField lastNameField;
+		private JButton searchJButton;
+		private TextArea listTextArea;
+		public  Search() {
+			this.setLayout(new BorderLayout());
+			lastNameField=new JTextField(10);
+			searchJButton=new JButton("Search");
+			listTextArea=new TextArea(25,25);
+			setSize(300,485);
+			setVisible(true);
+			setLocation(300,180);
+			JPanel upJPanel=new JPanel(new FlowLayout(2));
+			JPanel downJPanel=new JPanel();
+			upJPanel.setSize(300, 20);
+			upJPanel.add(lastNameField);
+			upJPanel.add(searchJButton);
+			downJPanel.add(listTextArea);
+			searchJButton.addActionListener(this);
+			this.add(upJPanel,BorderLayout.NORTH);
+			this.add(downJPanel,BorderLayout.SOUTH);
+		}
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			listTextArea.setText(myContactList.SearchbyLast(lastNameField.getText()));
+		}
+	}
+
 }
