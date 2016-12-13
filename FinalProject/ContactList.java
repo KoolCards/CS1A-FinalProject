8f494363db104f204c1e8a708454eeeebacca90b
ricardo <ricardoluliwei@gmail.com>
2016-12-12 20:24:24 -0800

    Add files via upload

---
+++ FinalProject/ContactList.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class ContactList implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2L;
+	/**
+	 * 
+	 */
+
+	private ArrayList<Person> list = new ArrayList<Person>();
+	public int PeopleNumber=list.size();
+	public ContactList() {}
+
+	public void addperson(Person newperson) {
+		list.add(newperson);
+		Collections.sort(list);
+	}
+	
+	public String toString() {
+		String tempString = "";
+		for (int i = 0; i < list.size(); i++) {
+			tempString += list.get(i) + "\n";
+		}
+		return tempString;
+	}
+
+	public void PrintContactList() {
+		System.out.println(toString());
+	}
+
+	public void clear() {
+		list.clear();
+	}
+	public String ShowName() {
+		String nameString="";
+		for (int i = 0; i < list.size(); i++) {
+			nameString+=list.get(i).getFirstName()+" "+list.get(i).getLastName()+":\n";
+		}
+		return nameString;
+	}
+	public String SearchbyLast(String newlastname) {
+		String personlistString = "";
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).getLastName().equalsIgnoreCase(newlastname)) {
+				personlistString += list.get(i) + "\n";
+			}
+		}
+		return personlistString;
+	}
+
+}
