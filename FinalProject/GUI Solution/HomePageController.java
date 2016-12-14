5e70284cbae116b2b33f012d5a920090797eafa2
krrapp <kevin.r.rapp@gmail.com>
2016-12-14 08:34:06 -0800

    Contains main to run GUI

---
+++ FinalProject/GUI Solution/HomePageController.java
@@ -0,0 +1,9 @@
+/**
+ * Starts the Graphical User Interface for the ContactList application. Displays buttons for User to begin
+ * operation of the ContactList
+ */
+public class HomePageController {
+	public static void main(String[] args) {
+		new HomePageView();
+	}
+}
